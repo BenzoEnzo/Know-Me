c528ec78aefb9a8f206f81e0dc125359251d3fcb
= <=>
2023-10-12 17:53:30 +0200

    userPanel in Progress

--- km-browser-gui/src/components/Userable.js
+++ km-browser-gui/src/components/Userable.js
@@ -4,10 +4,13 @@ const Userable = () => {
         <div className="extended-panel">
             <div className="left-container">
                 <div className="sub-container image-container">
-
+                        <h1>MIEJSCE NA ZDJECIE</h1>
+                </div>
+                <div className="name-container">
+                    <input placeholder="Zapisz Imię" />
                 </div>
                 <div className="sub-container description-container">
-
+                    <h1>MIEJSCE NA OPIS</h1>
                 </div>
             </div>
             <div className="right-container">
