3aeaf51bc58f908f3953fc760ee69f30b32de58b
= <=>
2023-10-12 16:05:39 +0200

    Second page in progress

--- km-browser-gui/src/components/Userable.js
+++ km-browser-gui/src/components/Userable.js
@@ -2,6 +2,17 @@ import './Userable.css';
 const Userable = () => {
     return (
         <div className="extended-panel">
+            <div className="left-container">
+                <div className="sub-container image-container">
+
+                </div>
+                <div className="sub-container description-container">
+
+                </div>
+            </div>
+            <div className="right-container">
+
+            </div>
         </div>
     );
 };
