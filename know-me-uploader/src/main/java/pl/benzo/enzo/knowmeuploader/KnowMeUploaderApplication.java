f6ea588130cc0fd3ecb3d2decb83d36edab1a2cd
= <=>
2023-10-24 07:31:12 +0200

    Photos uploader service

---
+++ know-me-uploader/src/main/java/pl/benzo/enzo/knowmeuploader/KnowMeUploaderApplication.java
@@ -0,0 +1,13 @@
+package pl.benzo.enzo.knowmeuploader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowMeUploaderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KnowMeUploaderApplication.class, args);
+    }
+
+}
